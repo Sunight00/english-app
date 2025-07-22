cbf3cc37578b3aa82f6a5163923e53bab6ef5e65
Sunight00 <ephraim.ewane@gmail.com>
2025-07-22 19:09:58 +0100

    go

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,4 +1,20 @@
 import image from '../public/images/b.svg';
+const body = document.querySelector('.body');
+function loadScreen(){
+    document.querySelector('.body').innerHTML = `
+    <div class="loadScreen">
+        <img id='loadImage' src="${image}" alt="Loading..." />
+        <div id="loading"></div>
+        <div class="spinner"></div>
+        <h2 class="loadingText dot-loader">Loading 
+        <span></span><span></span><span></span>
+        </h2>
+        <h1 class="intro">SUNIGHT GRAMMARLY</h1>
+    </div>
+    ` 
+}
 
-document.getElementById('loading').innerHTML = `
-    <img id='loadImage' src="${image}" alt="Loading..." />`;
+body.addEventListener(onload,loadScreen())
+
+
+
