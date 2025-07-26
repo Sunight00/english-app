636743b8fdcf5d03a060e47d7f62192affd30dd6
Sunight00 <ephraim.ewane@gmail.com>
2025-07-26 11:43:57 +0100

    okay

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,7 +1,10 @@
 import image from '../public/images/b.svg';
-const body = document.querySelector('.body');
+const load = document.querySelector('.load');
+let t = true;
 function loadScreen(){
-    document.querySelector('.body').innerHTML = `
+
+    if (t){
+    document.querySelector('.load').innerHTML = `
     <div class="loadScreen">
         <img id='loadImage' src="${image}" alt="Loading..." />
         <div id="loading"></div>
@@ -9,11 +12,20 @@ function loadScreen(){
         <h2 class="loadingText dot-loader">Loading 
         <span></span><span></span><span></span>
         </h2>
+        <h1 class="intro"></h1>
     </div>
-    ` 
+    ` }
 }
 
-body.addEventListener(onload,loadScreen())
+load.addEventListener(onload,loadScreen())
 
 
-
+setTimeout(endLoadScreen,4000)
+ function endLoadScreen(){
+    window.location.href ="./src/pages/home.html"
+    
+    /*document.querySelector('.loadScreen').remove()
+    homePage()
+    t = false;*/
+    homePage()
+ }
