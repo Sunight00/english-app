e1a56a0731b9e20579d64c1cc0e60e219d1d1c2d
Sunight00 <ephraim.ewane@gmail.com>
2025-08-05 11:43:24 +0100

    update

--- src/js/dictionary.mjs
+++ src/js/dictionary.mjs
@@ -3,18 +3,21 @@ export default class dictionary{
     {
         this.word = word
     }
-
+    //An api that returns random words on page reload
     async generateWord(){
         const response = await fetch (`https://random-word-api.vercel.app/api?words=1`)
         const object = await response.json()
         return object[0]
     }
+
+    // This accesses the return word from generateWord method and uses a dictionary api to get that word definitions
     async getWord(word){
         const response = await fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         const object = await response.json()
         return object
     }
 
+    //This Dom calls the generate and get word and and update DOM with the data
     async getWordInfo(){
         const word = await this.generateWord()
         const object= await this.getWord(word)
@@ -24,9 +27,11 @@ export default class dictionary{
         document.getElementById("phonetic").innerHTML = data.phonetic || 'phonetic not available'
         document.getElementById('audio').src = data.phonetics[0].audio || data.phonetics[1].audio
         document.getElementById("definition").innerHTML = `<br>${data.meanings[0].partOfSpeech}: ${data.meanings[0].definitions[0].definition} for more info <a href="https://dictionary.cambridge.org/dictionary/english/${word}" target='_blank'>Search</a>` 
-
     }
 
 }
+
+
+
 
 /* <br> ${data.meanings[0].partOfSpeech}: <br> 1. ${data.meanings[0].definitions[0].definition}<br> 2. ${data.meanings[1].definitions[1].definition}`*/
