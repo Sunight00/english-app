37dd55b253ad34b15cb702a88371cc59c8532163
Sunight00 <ephraim.ewane@gmail.com>
2025-08-05 11:17:33 +0100

    hh

--- src/js/dictionary.mjs
+++ src/js/dictionary.mjs
@@ -17,7 +17,7 @@ export default class dictionary{
         document.getElementById("word").innerHTML = data.word
         document.getElementById("phonetic").innerHTML = data.phonetic || 'phonetic not available'
         document.getElementById('audio').src = data.phonetics[0].audio || data.phonetics[1].audio
-        document.getElementById("definition").innerHTML = `<br>${data.meanings[0].partOfSpeech}: ${data.meanings[0].definitions[0].definition} for more info <a href="https://www.google.com/search?q=${word}&sca_esv=9576322275790996&sxsrf=AE3TifMW4TUSAuKLqmtiemawrBq0mO7dgg%3A1754271772749&source=hp&ei=HBCQaISdLI6jhbIPitPMoAw&iflsig=AOw8s4IAAAAAaJAeLM0zF55fpWHLPa57sZ70c6drJJYI&ved=0ahUKEwiEm-nOg_COAxWOUUEAHYopE8QQ4dUDCBc&uact=5&oq=happy&gs_lp=Egdnd3Mtd2l6IgVoYXBweTIEECMYJzIKEAAYgAQYQxiKBTILEAAYgAQYkQIYigUyDRAuGIAEGLEDGEMYigUyDRAuGIAEGEMY1AIYigUyChAAGIAEGEMYigUyDRAAGIAEGLEDGEMYigUyChAAGIAEGEMYigUyEBAAGIAEGLEDGEMYgwEYigUyChAAGIAEGEMYigVIjQ9QhgNYugxwAXgAkAEAmAHxAaAB7wiqAQMyLTW4AQPIAQD4AQGYAgagAqcJqAIKwgIQECMYgAQYJxiKBRjqAhitBsICDRAjGIAEGCcYigUY6gLCAgcQIxgnGOoCwgITEC4YgAQY0QMYxwEYJxiKBRjqAsICDxAjGIAEGCcYigUYRhj5AcICChAjGIAEGCcYigXCAg4QLhiABBixAxiDARiKBcICCBAAGIAEGLEDwgIFEC4YgATCAg4QABiABBixAxiDARiKBcICERAuGIAEGJECGMcBGIoFGK8BwgILEC4YgAQYsQMYgwGYAxHxBbyMGDeio9tNkgcFMS4wLjWgB9c5sgcDMi01uAeVCcIHBTItNC4yyAcw&sclient=gws-wiz">Search</a>` 
+        document.getElementById("definition").innerHTML = `<br>${data.meanings[0].partOfSpeech}: ${data.meanings[0].definitions[0].definition} for more info <a href="https://dictionary.cambridge.org/dictionary/english/${word}" target='_blank'>Search</a>` 
 
     }
 
